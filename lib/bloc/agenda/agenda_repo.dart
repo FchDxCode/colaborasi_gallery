5257e67e427a01a876cb869c421d5c722f858ab0
Fhrzl A. <121169138+Rhesheyz@users.noreply.github.com>
2024-09-06 13:33:04 +0700

    ijal gg gemink

--- lib/bloc/agenda/agenda_repo.dart
+++ lib/bloc/agenda/agenda_repo.dart
@@ -5,7 +5,7 @@ import 'agenda_models.dart';
 class AgendaRepository {
   Future<List<WisataJson>> fetchWisata() async {
     final response = await http.get(Uri.parse(
-        'https://ws.jakarta.go.id/gateway/DataPortalSatuDataJakarta/1.0/satudata?kategori=dataset&tipe=detail&url=indeks-kepuasan-layanan-penunjang-urusan-pemerintahan-daerah-pada-dinas-pariwisata-dan-ekonomi-kreatif'));
+        'https://praktikum-cpanel-unbin.com/api_bumi_fachru_fahrizal_alif_fatir/api/agenda'));
     if (response.statusCode == 200) {
       final List<dynamic> data = json.decode(response.body)['data'];
       return data.map((wisata) => WisataJson.fromJson(wisata)).toList();
