411dc7691b219147e66e1b25e25ddf55e12748f0
raihanalifp <132416154+nouzee@users.noreply.github.com>
2024-09-06 13:07:15 +0700

    Update tabScreens.dart

--- lib/screens/tabScreens.dart
+++ lib/screens/tabScreens.dart
@@ -1,41 +1,108 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
 import 'berandaScreens.dart';
 import 'infoScreens.dart';
 import 'galeriScreens.dart';
 import 'agendaScreens.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../bloc/info/info_bloc.dart';
+import '../bloc/info/info_repo.dart';
+
 import '../bloc/galeri/galeri_bloc.dart';
 import '../bloc/galeri/galeri_repo.dart';
 
+import '../bloc/agenda/agenda_bloc.dart';
+import '../bloc/agenda/agenda_repo.dart';
 
-class TabScreens extends StatelessWidget {
+class TabScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
       length: 4, // Number of tabs
       child: Scaffold(
         appBar: AppBar(
-          title: Text('DKI Jakarta Ope Data'),
-          backgroundColor: Colors.blue,
-          bottom: TabBar(
-            tabs: [
-              Tab(icon: Icon(Icons.home), text: 'Home'),
-              Tab(icon: Icon(Icons.heart_broken), text: 'Info'),
-              Tab(icon: Icon(Icons.airplanemode_active), text: 'Galeri'),
-              Tab(icon: Icon(Icons.person), text: 'Agenda'),
+          leading: IconButton(
+            icon: Icon(Icons.menu), // Ikon garis tiga
+            onPressed: () {
+              // Aksi saat ikon ditekan, biasanya membuka Drawer
+              Scaffold.of(context).openDrawer();
+            },
+          ),
+          title: Text('SMK NEGERI 4 BOGOR'),
+          centerTitle: true, // Agar judul berada di tengah
+          flexibleSpace: Container(
+            decoration: BoxDecoration(
+              gradient: LinearGradient(
+                begin: Alignment.topLeft,
+                end: Alignment.bottomRight,
+                colors: [Colors.blue, Colors.deepPurple], // Warna gradasi biru ke ungu
+              ),
+            ),
+          ),
+        ),
+        drawer: Drawer(
+          // Drawer menu, jika diinginkan bisa tambahkan item menu di sini
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: <Widget>[
+              DrawerHeader(
+                child: Text('Menu'),
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    begin: Alignment.topLeft,
+                    end: Alignment.bottomRight,
+                    colors: [Colors.blue, Colors.deepPurple], // Warna gradasi di drawer header
+                  ),
+                ),
+              ),
+              ListTile(
+                leading: Icon(Icons.home),
+                title: Text('Home'),
+                onTap: () {
+                  Navigator.pop(context); // Close the drawer
+                },
+              ),
+              // Tambahkan lebih banyak item di sini sesuai kebutuhan
             ],
           ),
         ),
         body: TabBarView(
           children: [
             BerandaScreens(),
-            InfoScreens(),
+            BlocProvider(
+              create: (context) => InfoBloc(repository: InfoRepository()),
+              child: InfoScreens(),
+            ),
             BlocProvider(
               create: (context) => GaleriBloc(repository: GaleriRepository()),
               child: GaleriScreens(),
             ),
-            AgendaScreens(),
+            BlocProvider(
+              create: (context) => AgendaBloc(repository: AgendaRepository()),
+              child: AgendaScreens(),
+            ),
           ],
+        ),
+        bottomNavigationBar: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.blue, Colors.deepPurple], // Gradasi pada tab bar bawah
+            ),
+          ),
+          child: TabBar(
+            tabs: [
+              Tab(icon: Icon(Icons.home), text: 'Home'),
+              Tab(icon: Icon(Icons.info), text: 'Info'),
+              Tab(icon: Icon(Icons.photo), text: 'Galeri'),
+              Tab(icon: Icon(Icons.event), text: 'Agenda'),
+            ],
+            labelColor: Colors.white, // Warna label ketika dipilih (kontras dengan gradasi)
+            unselectedLabelColor: Colors.white70, // Warna label ketika tidak dipilih (lebih terang)
+            indicatorColor: Colors.white, // Warna indikator tab
+          ),
         ),
       ),
     );
