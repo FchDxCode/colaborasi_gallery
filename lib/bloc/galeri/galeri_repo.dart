ec94a858158c60dc5701ff2aa8151d86a83c4569
Ryuqing~ <59086007+FchDxCode@users.noreply.github.com>
2024-09-06 14:25:25 +0700

    Update galeri_repo.dart

--- lib/bloc/galeri/galeri_repo.dart
+++ lib/bloc/galeri/galeri_repo.dart
@@ -2,14 +2,25 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'galeri_models.dart';
 
-class GaleriRepository {
+class GalleryRepository {
   Future<List<WisataJson>> fetchWisata() async {
-    final response = await http.get(Uri.parse('https://ws.jakarta.go.id/gateway/DataPortalSatuDataJakarta/1.0/satudata?kategori=dataset&tipe=detail&url=indeks-kepuasan-layanan-penunjang-urusan-pemerintahan-daerah-pada-dinas-pariwisata-dan-ekonomi-kreatif'));
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body)['data'];
-      return data.map((wisata) => WisataJson.fromJson(wisata)).toList();
-    } else {
-      throw Exception('Failed to load');
+    try {
+      final response = await http.get(Uri.parse('https://praktikum-cpanel-unbin.com/api_bumi_fachru_fahrizal_alif_fatir/api/gallery'));
+      
+      if (response.statusCode == 200) {
+        final Map<String, dynamic> jsonResponse = json.decode(response.body);
+        
+        if (jsonResponse.containsKey('data') && jsonResponse['data'] is List) {
+          final List<dynamic> data = jsonResponse['data'];
+          return data.map((wisata) => WisataJson.fromJson(wisata)).toList();
+        } else {
+          throw Exception('Unexpected JSON structure');
+        }
+      } else {
+        throw Exception('Failed to load: ${response.statusCode}');
+      }
+    } catch (e) {
+      throw Exception('Failed to load: $e');
     }
   }
 }
