053dc54ad50b7b05a91460c46855bad25c8b2a1e
Ryuqing~ <59086007+FchDxCode@users.noreply.github.com>
2024-09-06 13:49:54 +0700

    Update login_repo.dart

--- lib/bloc/login/login_repo.dart
+++ lib/bloc/login/login_repo.dart
@@ -1,11 +1,11 @@
-// login_repository.dart
+
 import 'package:http/http.dart' as http;
 import 'dart:convert';
 
 class LoginRepository {
   Future<void> login(String email, String password) async {
     final response = await http.post(
-      Uri.parse('http://127.0.0.1:8000/api/login'),
+      Uri.parse('https://praktikum-cpanel-unbin.com/api_bumi_fachru_fahrizal_alif_fatir/api/login'),
       headers: <String, String>{
         'Content-Type': 'application/json; charset=UTF-8',
       },
@@ -16,11 +16,8 @@ class LoginRepository {
     );
 
     if (response.statusCode == 200) {
-      // Login successful
       return;
     } else {
-      // If the server did not return a 200 OK response,
-      // then throw an exception.
       throw Exception('Failed to login');
     }
   }
