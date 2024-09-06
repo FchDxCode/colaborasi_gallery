a75cac379d1fe6c9d7a449ebb5724f7bd74a3ac9
Ryuqing~ <59086007+FchDxCode@users.noreply.github.com>
2024-09-06 14:25:42 +0700

    Update galeri_state.dart

--- lib/bloc/galeri/galeri_state.dart
+++ lib/bloc/galeri/galeri_state.dart
@@ -1,30 +1,31 @@
+
 import 'package:equatable/equatable.dart';
 import 'galeri_models.dart';
 
-abstract class GaleriState extends Equatable {
-  const GaleriState();
+abstract class GalleryState extends Equatable {
+  const GalleryState();
   
   @override
   List<Object> get props => [];
 }
 
-class GaleriInitial extends GaleriState {}
+class GalleryInitial extends GalleryState {}
 
-class GaleriLoading extends GaleriState {}
+class GalleryLoading extends GalleryState {}
 
-class GaleriLoaded extends GaleriState {
+class GalleryLoaded extends GalleryState {
   final List<WisataJson> wisatas;
 
-  const GaleriLoaded(this.wisatas);
+  const GalleryLoaded(this.wisatas);
 
   @override
   List<Object> get props => [wisatas];
 }
 
-class GaleriError extends GaleriState {
+class GalleryError extends GalleryState {
   final String message;
 
-  const GaleriError(this.message);
+  const GalleryError(this.message);
 
   @override
   List<Object> get props => [message];
