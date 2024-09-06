c6933a1461116f07490581a41385f4acb0a57c44
raihanalifp <132416154+nouzee@users.noreply.github.com>
2024-09-06 13:08:10 +0700

    Update infoScreens.dart

--- lib/screens/infoScreens.dart
+++ lib/screens/infoScreens.dart
@@ -1,46 +1,117 @@
 import 'package:flutter/material.dart';
-import 'package:http/http.dart' as http;
-import 'dart:convert';
-import '../bloc/info/info_repo.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/info/info_bloc.dart';
+import '../bloc/info/info_event.dart';
+import '../bloc/info/info_state.dart';
 
 class InfoScreens extends StatelessWidget {
-  Future<List<KesehatanJson>> fetchKesehatan() async {
-    final response =
-        await http.get(Uri.parse('https://ws.jakarta.go.id/gateway/DataPortalSatuDataJakarta/1.0/satudata?kategori=dataset&tipe=detail&url=perempuan-dan-anak-korban-kekerasan-yang-mendapatkan-layanan-kesehatan-oleh-tenaga-kesehatan-terlatih-di-puskesmas-mampu-tatalaksana-kekerasan-terhadap-perempuananak-ktpa-dan-pusat-pelayanan-terpadupusat-krisis-terpadu-pptpkt-di-rumah-sakit'));
-    if (response.statusCode == 200) {
-      final List<dynamic> data = json.decode(response.body)['data'];
-      return data.map((kesehatan) => KesehatanJson.fromJson(kesehatan)).toList();
-    } else {
-      throw Exception('Failed to load users');
-    }
-  }
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('User List'),
+        title: Text('Information'),
+        flexibleSpace: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [Colors.cyan, Colors.purple], // Gradasi cyan ke ungu
+            ),
+          ),
+        ),
       ),
-      body: FutureBuilder<List<KesehatanJson>?>(
-        future: fetchKesehatan(),
-        builder: (context, snapshot) {
-          if (snapshot.hasData) {
-            final kesehatans = snapshot.data;
-            return ListView.builder(
-              itemCount: kesehatans?.length ?? 0,
-              itemBuilder: (context, index) {
-                final kesehatan = kesehatans?[index];
-                return ListTile(
-                  title: Text(kesehatan?.periodeData ?? ''),
-                  subtitle: Text(kesehatan?.wilayah ?? ''),
-                );
-              },
-            );
-          } else if (snapshot.hasError) {
-            return Text('Error: ${snapshot.error}');
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            begin: Alignment.topLeft,
+            end: Alignment.bottomRight,
+            colors: [
+              Colors.cyan.withOpacity(0.5), // Warna cyan dengan transparansi 50%
+              Colors.purple.withOpacity(0.5) // Warna ungu dengan transparansi 50%
+            ],
+          ),
+        ),
+        child: BlocBuilder<InfoBloc, InfoState>(
+          builder: (context, state) {
+            if (state is InfoInitial) {
+              BlocProvider.of<InfoBloc>(context).add(FetchInfo());
+              return Center(child: CircularProgressIndicator());
+            } else if (state is InfoLoading) {
+              return Center(child: CircularProgressIndicator());
+            } else if (state is InfoLoaded) {
+              return ListView.builder(
+                padding: EdgeInsets.all(10), // Padding di sekitar ListView
+                itemCount: state.wisatas.length,
+                itemBuilder: (context, index) {
+                  final wisata = state.wisatas[index];
+                  return Padding(
+                    padding: const EdgeInsets.symmetric(vertical: 8.0), // Padding antar box
+                    child: Container(
+                      decoration: BoxDecoration(
+                        gradient: LinearGradient(
+                          begin: Alignment.topLeft,
+                          end: Alignment.bottomRight,
+                          colors: [
+                            Colors.blue[100]!.withOpacity(0.8), // Gradasi biru muda dengan transparansi
+                            Colors.deepPurple[100]!.withOpacity(0.8) // Gradasi ungu muda dengan transparansi
+                          ],
+                        ),
+                        borderRadius: BorderRadius.circular(12), // Membuat sudut rounded
+                        boxShadow: [
+                          BoxShadow(
+                            color: Colors.grey.withOpacity(0.5), // Warna shadow
+                            spreadRadius: 2,
+                            blurRadius: 5,
+                            offset: Offset(0, 3), // Posisi shadow
+                          ),
+                        ],
+                      ),
+                      child: Stack(
+                        children: [
+                          // Bagian ListTile
+                          ListTile(
+                            contentPadding: EdgeInsets.all(16), // Padding dalam ListTile
+                            title: Text(
+                              wisata.periodeData ?? '',
+                              style: TextStyle(
+                                fontWeight: FontWeight.bold,
+                                color: Colors.blueAccent, // Warna teks judul
+                              ),
+                            ),
+                            subtitle: Text(
+                              wisata.triwulan1 ?? '',
+                              style: TextStyle(
+                                color: Colors.black54, // Warna teks pertama
+                              ),
+                            ),
+                            leading: Icon(Icons.info, color: Colors.blue), // Ikon di bagian kiri
+                            trailing: Icon(Icons.arrow_forward_ios, color: Colors.blue), // Ikon di bagian kanan
+                          ),
+                          
+                          // Posisi untuk nilai1 di pojok kanan bawah
+                          Positioned(
+                            bottom: 10,
+                            right: 16,
+                            child: Text(
+                              wisata.nilai1 ?? '',
+                              style: TextStyle(
+                                color: Colors.grey, // Warna teks abu-abu
+                                fontSize: 14,
+                              ),
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  );
+                },
+              );
+            } else if (state is InfoError) {
+              return Center(child: Text('Error: ${state.message}'));
+            }
+            return Container();
+          },
+        ),
       ),
     );
   }
