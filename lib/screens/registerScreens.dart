f5016140b75e906ac8b302156add14220c592f60
edogawafatir <edogawafatir@gmail.com>
2024-09-07 04:33:16 +0000

    test

--- lib/screens/registerScreens.dart
+++ lib/screens/registerScreens.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
-import '../widgets/form_register.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import '../bloc/register/regist_bloc.dart';
+import '../bloc/register/register_state.dart';
 import '../widgets/logo.dart';
+import '../widgets/form_register.dart';
+import 'tabScreens.dart';
 
 class RegisterScreen extends StatelessWidget {
   const RegisterScreen({super.key});
@@ -10,30 +14,39 @@ class RegisterScreen extends StatelessWidget {
     final bool isSmallScreen = MediaQuery.of(context).size.width < 600;
 
     return Scaffold(
-      body: Center(
-        child: isSmallScreen
-            ? const Column(
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  LogoWidget(),
-                  RegisterFormWidget(),
-                ],
-              )
-            : Container(
-                padding: const EdgeInsets.all(32.0),
-                constraints: const BoxConstraints(maxWidth: 800),
-                child: const Row(
-                  children: [
-                    Expanded(child: LogoWidget()),
-                    Expanded(
-                      child: Center(child: RegisterFormWidget()),
-                    ),
-                  ],
+      body: BlocListener<RegisterBloc, RegisterState>(
+        listener: (context, state) {
+          if (state is RegisterSuccess) {
+            // Navigate to TabScreens
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => TabScreen()),
+            );
+          } else if (state is RegisterFailure) {
+            ScaffoldMessenger.of(context).showSnackBar(
+              SnackBar(content: Text(state.error)),
+            );
+          }
+        },
+        child: Center(
+          child: isSmallScreen
+              ? const Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [LogoWidget(), RegisterFormWidget()],
+                )
+              : Container(
+                  padding: const EdgeInsets.all(32.0),
+                  constraints: const BoxConstraints(maxWidth: 800),
+                  child: const Row(
+                    children: [
+                      Expanded(child: LogoWidget()),
+                      Expanded(
+                        child: Center(child: RegisterFormWidget()),
+                      ),
+                    ],
+                  ),
                 ),
-              ),
+        ),
       ),
     );
   }
 }
-
-
